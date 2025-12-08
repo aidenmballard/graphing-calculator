84e41b0ea26b81ca8917c0a6a45330b132576cff
aidenmballard <132618849+aidenmballard@users.noreply.github.com>
2025-12-08 13:54:23 +0000

    i lied, now it simplifies

--- Equation.java
+++ Equation.java
@@ -24,25 +24,37 @@ public class Equation {
         simplify(tokens);
     }
 
+    // Simplifies an expression of numbers to one constant
     public double simplify(List<Token> tokens) {
+        // Creates the coefficient value, index, and operation index
         double coefficient = 0;
-        int index = 0;
-        int operationIndex = 0;
+        int index = 0, operationIndex = 0;
+
+        // Loops until there is only one token left
         while (tokens.size() > 1) {
+            // Gets the token at the current index and checks if it is an operation
             Token token = tokens.get(index);
             if (token.type.equals("OPERATION")) {
+                // Checks if the current operation is the same as the operation at
+                // operationIndex
                 if (token.value.equals(String.valueOf(Token.operations[operationIndex])))
+                    // Evaluates the operation in accordance to PEDMAS
                     if (token.value.equals("(")) {
+                        // Finds the right parenthesis index
                         int rightParenIndex = 0;
-                        for (Token token2 : tokens)
-                            if (token2.value.equals(")")) {
-                                rightParenIndex = tokens.indexOf(token2);
+                        for (int i = index + 1; i < tokens.size(); i++) {
+                            if (tokens.get(i).value.equals(")")) {
+                                rightParenIndex = i;
                                 break;
                             }
+                        }
+
+                        // Simplifies the expression inside the parentheses and sets that value
                         List<Token> subTokens = tokens.subList(index + 1, rightParenIndex);
                         tokens.set(index + 1, new Token("NUMBER", Double.toString(simplify(subTokens))));
                     } else if (token.value.equals(")")) {
-                        if (tokens.get(index + 1).type.equals("NUMBER")) {
+                        // Checks if the next charater is a number for implicit multiplication
+                        if (index + 1 < tokens.size() && tokens.get(index + 1).type.equals("NUMBER")) {
                             tokens.set(index + 1, new Token("NUMBER",
                                     Double.toString(Double.parseDouble(tokens.get(index - 1).value)
                                             * Double.parseDouble(tokens.get(index + 1).value))));
@@ -53,30 +65,35 @@ public class Equation {
                             tokens.remove(index);
                             tokens.remove(index - 2);
                         }
+                        // Evaluates exponents
                     } else if (token.value.equals("^")) {
                         tokens.set(index - 1, new Token("NUMBER",
                                 Double.toString(Math.pow(Double.parseDouble(tokens.get(index - 1).value),
                                         Double.parseDouble(tokens.get(index + 1).value)))));
                         tokens.remove(index + 1);
                         tokens.remove(index);
+                        // Evaluates multiplication
                     } else if (token.value.equals("*")) {
                         tokens.set(index - 1, new Token("NUMBER",
                                 Double.toString(Double.parseDouble(tokens.get(index - 1).value)
                                         * Double.parseDouble(tokens.get(index + 1).value))));
                         tokens.remove(index + 1);
                         tokens.remove(index);
+                        // Evaluates division
                     } else if (token.value.equals("/")) {
                         tokens.set(index - 1, new Token("NUMBER",
                                 Double.toString(Double.parseDouble(tokens.get(index - 1).value)
                                         / Double.parseDouble(tokens.get(index + 1).value))));
                         tokens.remove(index + 1);
                         tokens.remove(index);
+                        // Evaluates addition
                     } else if (token.value.equals("+")) {
                         tokens.set(index - 1, new Token("NUMBER",
                                 Double.toString(Double.parseDouble(tokens.get(index - 1).value)
                                         + Double.parseDouble(tokens.get(index + 1).value))));
                         tokens.remove(index + 1);
                         tokens.remove(index);
+                        // Evaluates subtraction
                     } else if (token.value.equals("-")) {
                         tokens.set(index - 1, new Token("NUMBER",
                                 Double.toString(Double.parseDouble(tokens.get(index - 1).value)
@@ -86,14 +103,19 @@ public class Equation {
                     }
             }
 
+            // Incements the index and operation index appropriately
             index++;
             if (index >= tokens.size()) {
                 index = 0;
                 operationIndex++;
             }
+
+            // Breaks the loop if all operations have been evaluated
             if (operationIndex >= Token.operations.length)
                 break;
         }
+
+        // Returns the final token, which is the simplified value
         System.out.println("Printing tokens: " + tokens);
         coefficient = Double.parseDouble(tokens.get(0).value);
         System.out.println(coefficient);
